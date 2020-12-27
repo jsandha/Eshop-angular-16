d269a7e1c4144dbbddf8759d1fac338322c9e5e6
Jaskaran Singh <jsandha@gmail.com>
2020-12-27 16:43:16 -0500

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDE92CJwZEYgkMITBhc3e7EPxt09YO16sE",
+    authDomain: "eshop-5bc95.firebaseapp.com",
+    projectId: "eshop-5bc95",
+    storageBucket: "eshop-5bc95.appspot.com",
+    messagingSenderId: "378405337280",
+    appId: "1:378405337280:web:af221b98760a2a3d7b9525",
+    measurementId: "G-4WZM5CKRD7"
+  }
 };
 
 /*
