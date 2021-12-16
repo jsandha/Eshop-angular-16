c8fcf4c0d31136d4771d8ea59c40fd3a4c9d6b63
jaskaran singh <jsandha@gmail.com>
2021-12-16 13:21:18 -0500

    all working angular 13

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,42 +1,41 @@
 import { AppUser } from './../models/app-user';
 import { switchMap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { UserService } from './user.service';
 
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>;
 
+  constructor(
+    private afAuth: AngularFireAuth,
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {
+    this.user$ = afAuth.authState;
+  }
 
-constructor(
-      private afAuth: AngularFireAuth,
-      private route: ActivatedRoute,
-      private userService: UserService) {
-      this.user$ = afAuth.authState;
-   }
-
-login(){
-    const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';   // it copy the url on the address bar
-  localStorage.setItem('returnUrl', returnUrl);                                 // stores url in localstorage
+  login() {
+    const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/'; // it copy the url on the address bar
+    localStorage.setItem('returnUrl', returnUrl); // stores url in localstorage
 
     this.afAuth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
-logout(){
-  this.afAuth.signOut();
+  logout() {
+    this.afAuth.signOut();
   }
 
-get appUser$(): Observable<AppUser>{
-  return this.user$
-  .pipe(
-    switchMap(user => {
-      if (user) return this.userService.get(user.uid); // this and next statement= if there is no user then there is no observable so error with swtichmap. so we retun observable of null
-       return of(null);
-    }
-      ))
+  get appUser$(): Observable<AppUser> {
+    return this.user$.pipe(
+      switchMap((user) => {
+        if (user) return this.userService.get(user.uid); // this and next statement= if there is no user then there is no observable so error with switchmap. so we return observable of null
+        return of(null);
+      })
+    );
   }
 }
--- src/app/shared/services/shopping-cart.service.ts
+++ src/app/shared/services/shopping-cart.service.ts
@@ -1,64 +1,79 @@
 import { Product } from './../models/product';
 import { ShoppingCart } from 'src/app/shared/models/shopping-cart';
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireObject } from '@angular/fire/database';
-import { map, mapTo, pluck, take} from 'rxjs/operators';
+import {
+  AngularFireDatabase,
+  AngularFireObject,
+} from '@angular/fire/compat/database';
+import { map, mapTo, pluck, take } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 @Injectable()
 export class ShoppingCartService {
   quantity: number;
 
-constructor(private db: AngularFireDatabase ) {}
+  constructor(private db: AngularFireDatabase) {}
 
-private create(){
-   return this.db.list('shopping-cart/').push(
-     {dateCreated: new Date().toTimeString()}
-     )
+  private create() {
+    return this.db
+      .list('shopping-cart/')
+      .push({ dateCreated: new Date().toTimeString() });
   }
 
-private getItem(cartId: string, productId: string){
-  return this.db.object('shopping-cart/' + cartId + '/items/' + productId);
-}
-
-private async getOrCreateCartId(): Promise<string>{
-  const cartId = localStorage.getItem('cartId');
-  if (cartId) return cartId;
-      const result = this.create();
-      localStorage.setItem('cartId', result.key);
-      return  result.key;
+  private getItem(cartId: string, productId: string) {
+    return this.db.object('shopping-cart/' + cartId + '/items/' + productId);
   }
-;
-private async updateItem(product: Product, change: number){
+
+  private async getOrCreateCartId(): Promise<string> {
+    const cartId = localStorage.getItem('cartId');
+    if (cartId) return cartId;
+    const result = this.create();
+    localStorage.setItem('cartId', result.key);
+    return result.key;
+  }
+  private async updateItem(product: Product, change: number) {
     let quantity;
-    const cartId: string = await this.getOrCreateCartId();// get the id
-    const itemRef: AngularFireObject<unknown> = this.getItem(cartId, product.$key); //get iteRef to the product
+    const cartId: string = await this.getOrCreateCartId(); // get the id
+    const itemRef: AngularFireObject<unknown> = this.getItem(
+      cartId,
+      product.$key
+    ); //get iteRef to the product
     const item$: Observable<unknown> = itemRef.valueChanges(); // get observable of item
-    item$.pipe(take(1)).subscribe(item => {
-      (item === null)? quantity = 1 : quantity = (item['quantity'] || 0) + change
+    item$.pipe(take(1)).subscribe((item) => {
+      item === null
+        ? (quantity = 1)
+        : (quantity = (item['quantity'] || 0) + change);
       if (quantity === 0) itemRef.remove();
-      else itemRef.update({
+      else
+        itemRef.update({
           title: product.title,
           imageUrl: product.imageUrl,
           price: product.price,
-          quantity: quantity});
+          quantity: quantity,
+        });
     });
   }
 
-async getCart(): Promise<Observable<ShoppingCart>>{
+  async getCart(): Promise<Observable<ShoppingCart>> {
     const cartId: string = await this.getOrCreateCartId();
-   return this.db.list('shopping-cart/' + cartId).valueChanges().pipe(
-   map(cart => new ShoppingCart(cart['1'])))
+    return this.db
+      .list('shopping-cart/' + cartId)
+      .valueChanges()
+      .pipe(map((cart) => new ShoppingCart(cart['1'] as {})));
   }
 
-// yet to do.. have to return the object new shoppingcart
-async getOrderCart(id){
-  this.db.list('orders/' + id).valueChanges().pipe(
-    take(1),
-    pluck('1'),
-    map(x=> console.log(x['product']))
+  // yet to do.. have to return the object new shoppingcart
+  async getOrderCart(id) {
+    this.db
+      .list('orders/' + id)
+      .valueChanges()
+      .pipe(
+        take(1),
+        pluck('1'),
+        map((x) => console.log(x['product']))
 
-    // map(x=>  new ShoppingCart[x])
-    ).subscribe(x=> console.log(x))
+        // map(x=>  new ShoppingCart[x])
+      )
+      .subscribe((x) => console.log(x));
     // map(cart => { cart['1'].forEach(el => {
     //           (el.product).reduce((acc,obj)=> {
     //             // acc[obj] = obj
@@ -73,23 +88,23 @@ async getOrderCart(id){
     //   })
     //  });
     // })
-  // .subscribe(x=>console.log(x))
+    // .subscribe(x=>console.log(x))
 
-      // map(cart => new ShoppingCart(cart['1'])))
-// update the cart and the objects related to it
+    // map(cart => new ShoppingCart(cart['1'])))
+    // update the cart and the objects related to it
     // updateItems()
-}
+  }
 
-async addToCart(product: Product){
+  async addToCart(product: Product) {
     this.updateItem(product, 1);
   }
 
-async removeFromCart(product: Product){
+  async removeFromCart(product: Product) {
     this.updateItem(product, -1);
   }
 
-async clearCart(){
-  const cartId = await this.getOrCreateCartId();
-  this.db.object('/shopping-cart/' + cartId + '/items').remove();
+  async clearCart() {
+    const cartId = await this.getOrCreateCartId();
+    this.db.object('/shopping-cart/' + cartId + '/items').remove();
   }
 }
