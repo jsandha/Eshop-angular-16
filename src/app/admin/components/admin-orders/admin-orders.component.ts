312535097bfc03357fc9dc11756bda5f48e4d2a3
jaskaran singh <jsandha@gmail.com>
2024-07-20 17:30:45 -0400

    removed data tables

--- src/app/admin/components/admin-orders/admin-orders.component.ts
+++ src/app/admin/components/admin-orders/admin-orders.component.ts
@@ -8,14 +8,11 @@ import { Subject, Subscription } from 'rxjs';
 })
 export class AdminOrdersComponent {
   orders$;
-  dtOptions: DataTables.Settings = {};
-  dtTrigger: Subject<any> = new Subject<any>();
   subscription: Subscription;
 
   constructor(private orderService: OrderService) {
     this.subscription = this.orderService.getAllOrders().subscribe((x) => {
       this.orders$ = x;
-      this.dtTrigger.next();
     });
   }
 
